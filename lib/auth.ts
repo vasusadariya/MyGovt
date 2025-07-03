b16fb1140fa1af537926698c3c34b0e4f6354131
Vasu <sadariyavasu5@gmail.com>
2025-07-03 13:58:39 +0530

    solve the mongo error

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server"
 import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google"
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import bcrypt from "bcryptjs"
 import clientPromise from "./mongodb"
 
